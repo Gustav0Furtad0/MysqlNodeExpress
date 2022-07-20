2701ea0512c87e10b17e6bebb94a55479a3f9f53
Gustav0Furtad0 <gufurtado02@gmail.com>
2022-07-20 10:07:56 -0300

    metodos http expressjs

--- index.js
+++ index.js
@@ -1,31 +1,6 @@
 'use strict';
 
-var express = require('express');
-var app = express();
-var path = require('path');
-var PORT = 3000;
- 
-app.use(express.static('public'))
+const rotas = require('./server/routes/enter.js');
 
-// Without middleware
-app.get('/', function(req, res, next){
-    var options = {
-        root: path.join(__dirname)
-    };
-     
-    var fileName = 'public/html/home.html';
-    res.sendFile(fileName, options, function (err) {
-        if (err) {
-            next(err);
-        } else {
-            console.log('Sent:', fileName);
-        }
-    });
-});
+rotas.enviar('html/home.html');
 
-
- 
-app.listen(PORT, function(err){
-    if (err) console.log(err);
-    console.log("Server listening on PORT", PORT);
-});
---
+++ server/routes/enter.js
@@ -0,0 +1,44 @@
+'use strict';
+
+var express = require('express');
+var app = express();
+var path = require('path');
+
+
+
+module.exports = {
+    
+    enviar(fileName) {
+        var raiz = path.dirname(process.mainModule.filename)
+
+        app.use(express.static(raiz + 'public'));
+
+        app.get('/', (req, res, next) => {
+            var options = {
+                root: path.join(raiz + '/public/')
+            };
+
+            console.log(req.url);
+
+            res.sendFile(fileName, options, (err) => {
+                if (err)
+                    next(err);
+                else
+                    console.log('Enviado:', fileName);  
+            });
+
+        });
+
+        app.get('/nome', (req, res) => {
+            let  nome = req.query.nome;
+            console.log(nome);
+            res.send(`O nome é ${nome}`);
+        });
+
+        app.listen(3000, (err) => {
+            if (err) console.log(err);
+            console.log("Servidor rodando na porta", 3000);
+        });
+    }
+    
+}
