0e20b703772f72da171212ae653e047bdb922031
Gustav0Furtad0 <gufurtado02@gmail.com>
2022-07-20 12:33:41 -0300

    insercao direta node mysql

--- server/database/index.js
+++ server/database/index.js
@@ -12,7 +12,7 @@ var con = mysql.createConnection({
 con.connect((err) => {if (err) throw err});
 
 const pullAllUsers = () => {
-    let sql = 'SELECT * FROM users';
+    let sql = 'SELECT * FROM users ORDER BY id';
     con.query(sql, async (err, result) => {
         if (err) throw (err);
         await result.forEach(element => {
@@ -22,14 +22,53 @@ const pullAllUsers = () => {
     });
 };
 
-const insereUser = async (id, nome, senha) => {
+const insereUser = (id, nome, senha) => {
     let sql = `INSERT INTO users (id, nome, passwrod) VALUES (${id}, '${nome}', '${senha}')`;
     con.query(sql, (err) => {
         if (err) throw err;
-        console.log(`Usuário de id ${id} inserido com sucesso`);
+        console.log(`Usuário de id ${id} INSERIDO com sucesso`);
     });
 };
 
-pullAllUsers();
+const deleteUser = (id) => {
+    let sql = `DELETE FROM users WHERE id=${id}`;
+    con.query(sql, (err) => {
+        if(err) throw err;
+        console.log(`Usuário de id ${id} DELETADO com sucesso`)
+    });
+}
 
-insereUser(4, 'Teste', '123456');
+const updateUser = (id, set, value) => {
+    let sql;
+    if (typeof set === 'string') {
+        sql = `UPDATE users SET ${set}='${value}' WHERE id=${id}`;
+    } else {
+        sql = `UPDATE users SET ${set}=${value} WHERE id=${id}`;
+    }
+    con.query(sql, (err) => {
+        if(err) throw err;
+        console.log(`Update de ${set} feito com sucesso em id=${id}`);
+    });
+}
+
+const dados = process.argv.slice();
+switch (dados[2]){
+    case 'mostrar':
+        pullAllUsers();
+        break;
+
+    case 'insere':
+        break;
+
+    case 'update':
+        break;
+
+    case 'delete':
+        break;
+    
+    default:
+        console.log("Valor definido para opereação inválido");
+}
+
+
+con.end();
